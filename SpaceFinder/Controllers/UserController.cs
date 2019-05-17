66345d0c463ccb0986c95e2b7b44f342ad24191a
Akash Patel <akash.patel@multidots.com>
2019-05-17 19:08:47 +0530

    Initial commit

---
+++ SpaceFinder/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using SpaceFinder.Entity;
+using SpaceFinder.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpaceFinder.Controllers
+{
+    public class UserController : Controller
+    {
+        private IUserRepository _userRepository;
+
+        public UserController()
+        {
+            _userRepository = new UserRepository(new SpaceFinderEntities1());
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ SpaceFinder/Repository/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using SpaceFinder.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpaceFinder.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private SpaceFinderEntities1 _context;
+
+        public UserRepository(SpaceFinderEntities1 userContext)
+        {
+            _context = userContext;
+        }
+    }
+}
